5ef98fdec958130a92aa40006ddd9daba36b4d12
Laslo Hunhold <dev@frign.de>
2022-08-16 16:51:17 +0200

    Use SIZE_MAX instead of (size_t)(-1)
    Thanks Mattias for pointing this out!
    
    Signed-off-by: Laslo Hunhold <dev@frign.de>

--- gen/util.c
+++ gen/util.c
@@ -34,7 +34,7 @@ struct break_test_payload
 static void *
 reallocate_array(void *p, size_t len, size_t size)
 {
-	if (len > 0 && size > (size_t)(-1) / len) {
+	if (len > 0 && size > SIZE_MAX / len) {
 		errno = ENOMEM;
 		return NULL;
 	}
