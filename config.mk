cd3a639d18c25942d0d48c8001f18222ba5899ef
Laslo Hunhold <dev@frign.de>
2022-10-25 17:16:21 +0200

    Move comments on macro-definition-lines to separate lines
    The standard says
    
    	Macro definitions are in the form:
    
    		string1 = [string2]
    
    	The macro named string1 is defined as having the value of
    	string2, where string2 is defined as all characters, if any,
    	after the <equals-sign>, up to a comment character ( '#' ) or
    	an unescaped <newline>. Any <blank> characters immediately
    	before or after the <equals-sign> shall be ignored.
    
    and thus having a declaration like
    
    	MACRO = helloworld # comment
    
    yields with MACRO containing the value "helloworld ", which is obviously
    undesired for path-declarations. This is fixed now.
    
    Thanks to Ionen Wolkens for reporting this issue!
    
    Signed-off-by: Laslo Hunhold <dev@frign.de>

--- config.mk
+++ config.mk
@@ -1,12 +1,12 @@
 # Customize below to fit your system (run ./configure for automatic presets)
 
-# paths
+# paths (unset $PCPREFIX to not install a pkg-config-file)
 DESTDIR   =
 PREFIX    = /usr/local
 INCPREFIX = $(PREFIX)/include
 LIBPREFIX = $(PREFIX)/lib
 MANPREFIX = $(PREFIX)/share/man
-PCPREFIX  = $(LIBPREFIX)/pkgconfig # unset to not install a pkg-config-file
+PCPREFIX  = $(LIBPREFIX)/pkgconfig
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE
@@ -23,10 +23,10 @@ SOFLAGS   = -shared -nostdlib -Wl,--soname=libgrapheme.so.$(VERSION_MAJOR).$(VER
 SONAME    = libgrapheme.so.$(VERSION_MAJOR).$(VERSION_MINOR).$(VERSION_PATCH)
 SOSYMLINK = true
 
-# tools
+# tools (unset $LDCONFIG to not call ldconfig(1) after install/uninstall)
 CC       = cc
 BUILD_CC = $(CC)
 AR       = ar
 RANLIB   = ranlib
-LDCONFIG = ldconfig # unset to not call ldconfig(1) after install/uninstall
+LDCONFIG = ldconfig
 SH       = sh
